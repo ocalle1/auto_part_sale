312a60c8005fe7528d8a926d03e1e7145c95e008
Oscar C <ocalle374@gmail.com>
2025-07-27 10:16:10 -0500

    first commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>OC Auto Part Sales</h1>
+      <button>Home</button>
+      <button>Contact</button>
+    </header>
+  );
+}
+
+export default Header;
