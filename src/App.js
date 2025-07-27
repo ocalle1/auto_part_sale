66f9994a3f83d34de46fa56c7c204af370024d36
Oscar Calle <ocalle374@gmail.com>
2025-07-27 10:40:25 -0500

    image update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './Header';
+import Images from './Images';
 
 function App() {
   return (
     <div>
       <Header />
+    <Images />
+
     </div>
   
   );
---
+++ src/Images.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from "react";
+import transmission from "./assets/transmission.jpg";
+import engne from "./assets/engine.png";
+import door from "./assets/door.jpg";
+
+const images = [transmission, engne, door];
+//work on the transion of the images
+function Images() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+return() => clearInterval(timer);
+  }, []);
+return (
+    <div style={{ textAlign: "center" }}>
+      <img
+        src={images[currentIndex]}
+        alt={`Slide ${currentIndex}`}
+        style={{ width: "400px", height: "auto", borderRadius: "12px" }}
+      />
+      <div style={{ marginTop: "10px" }}>
+        {images.map((_, i) => (
+          <button
+            key={i}
+            onClick={() => setCurrentIndex(i)}
+            style={{
+              margin: "0 4px",
+              padding: "5px",
+              borderRadius: "50%",
+              background: currentIndex === i ? "#333" : "#ccc",
+              border: "none",
+              width: "12px",
+              height: "12px",
+              cursor: "pointer",
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Images;  
